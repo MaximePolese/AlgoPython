c35ebfc9bf788ad858af30c6f2036387bcdb6a10
maxime.polese <maxime.polese@le-campus-numerique.fr>
2024-02-12 13:29:49 +0100

    shortest path ok

--- pvc.py
+++ pvc.py
@@ -1,8 +1,7 @@
 import haversine as hs
-import numpy as np
 import folium
 
-allPoints = [
+all_points = [
     [45.171112, 5.695952],
     [45.183152, 5.699386],
     [45.174115, 5.711106],
@@ -30,47 +29,89 @@ allPoints = [
 ]
 
 
-def display_all_points():
-    for point in allPoints:
-        for coordinate in point:
-            print(coordinate)
-
-
-def calc_distance(pointA, pointB, all_points):
+def calc_distance_between_two_points(pointA, pointB, all_points):
     loc1 = (all_points[pointA][0], all_points[pointA][1])
     loc2 = (all_points[pointB][0], all_points[pointB][1])
     result = hs.haversine(loc1, loc2)
     return result
 
 
-print(calc_distance(0, 1, allPoints))
-
-a = np.array([[1], [5], [9]])
-print(a[1][0])
-
-
-# def calc_all_distances(allPoints):
-#     distances = np.array([[0], [1]])
-#     for i in range(0, len(allPoints)):
-#         # distances[i] = []
-#         for j in range(i, len(allPoints)):
-#             if i == j:
-#                 distances[[i], [j]] = 0
-#             else:
-#                 distances[[i], [j]] = calc_distance(i, j, allPoints)
-#             print(f"La distance entre les points {i} et {j} est égale à {distances[i][j]} km")
-
-def calc_all_distances(allPoints):
-    for i in range(0, len(allPoints)):
-        for j in range(i, len(allPoints)):
+def calc_distances_between_all_points(all_points):
+    distances = []
+    for i in range(0, len(all_points)):
+        row = []
+        for j in range(0, len(all_points)):
             if i == j:
                 result = 0
             else:
-                result = calc_distance(i, j, allPoints)
-                print(f"La distance entre les points {i} et {j} est égale à {result} km")
+                result = calc_distance_between_two_points(i, j, all_points)
+            row.append(result)
+            # print(f"La distance entre les points {i} et {j} est égale à {result} km")
+        distances.append(row)
+    return distances
 
 
-calc_all_distances(allPoints)
+distances = calc_distances_between_all_points(all_points)
+path = []
+visited = [False] * len(all_points)
 
-m = folium.Map(location=(45.171112, 5.695952))
+
+def find_next_move(last_point):
+    shortest_way = 1000
+    next_index = 1000
+    dist_total = 0
+    for i in range(0, len(all_points)):
+        if distances[last_point][i] > 0 and not visited[i]:
+            if distances[last_point][i] < shortest_way:
+                shortest_way = distances[last_point][i]
+                next_index = i
+    visited[last_point] = True
+    dist_total += shortest_way
+    path.append(next_index)
+    return next_index
+
+
+def find_shortest_path(start_point):
+    path.append(start_point)
+    next_point = start_point
+    for i in range(0, len(all_points) - 1):
+        next_point = find_next_move(next_point)
+
+
+find_shortest_path(0)
+print(path)
+
+m = folium.Map([45.18486504179179, 5.731181509376984], zoom_start=13)
+folium.Marker(
+    location=[45.18486504179179, 5.731181509376984],
+    tooltip="Le Campus Numérique",
+    # popup="Le Campus Numérique",
+    icon=folium.Icon(color="purple", icon=""),
+).add_to(m)
+
+
+def display_points(all_points):
+    for i in range(0, len(all_points)):
+        folium.Marker(
+            location=[all_points[i][0], all_points[i][1]],
+            tooltip=i,
+            icon=folium.Icon(color="green", icon=""),
+        ).add_to(m)
+
+
+display_points(all_points)
+
+path_coords = []
+
+
+def display_path(path):
+    for i in range(0, len(path)):
+        path_coords.append([all_points[path[i]][0], all_points[path[i]][1]])
+
+
+display_path(path)
+print(path_coords)
+
+folium.PolyLine(path_coords, tooltip="shortest_path").add_to(m)
+
 m.save("index.html")
